3cfa7f788287c75aa86370417e8e4770982bb1d3
iRuxu <rx6@qq.com>
2020-07-21 02:22:47 +0800

    添加私有状态图标

--- src/components/single-header.vue
+++ src/components/single-header.vue
@@ -5,6 +5,7 @@
         <div class="m-single-title">
             <a class="u-title u-sub-block" :href="url">
                 <i v-if="isOriginal" class="u-original">原创</i> 
+                <img v-if="isPrivate" class="u-private" svg-inline src="../assets/img/single/lock.svg" title="仅自己可见"/>
                 <span class="u-title-text">{{ title }}</span>
             </a>
         </div>
@@ -73,6 +74,10 @@ export default {
         },
         isOriginal:function (){
             return !!~~_.get(this.post, "original")
+        },
+        isPrivate : function (){
+            let status = _.get(this.post, "post_status")
+            return status == 'draft' || status == 'pending'
         },
         title: function() {
             return _.get(this.post, "post_title") || "无标题";
