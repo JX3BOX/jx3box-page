deb48ba84712f26a28477b2a7f36a06c103391ad
iRuxu <rx6@qq.com>
2021-08-16 16:44:41 +0800

    update share widget

--- src/components/single-panel.vue
+++ src/components/single-panel.vue
@@ -3,44 +3,56 @@
         <div class="u-minigroup">
             <Print class="u-fn" :title="title" :authorID="authorID" />
             <QRcode class="u-fn" />
-            <Sharing class="u-fn" :title="title" :pic="banner"/>
+            <Share class="u-fn u-share" :meta="meta" :simple="true"/>
         </div>
         <div class="u-btngroup">
-            <Fav class="u-fav" :postType="post.post_type" :postId="post.ID"/>
-            <Feed class="u-feed" :postType="post.post_type" :postId="post.ID"/>
-            <Adminbutton class="u-admin" size="mini"/>
+            <Fav class="u-fav" :postType="post.post_type" :postId="post.ID" />
+            <Feed class="u-feed" :postType="post.post_type" :postId="post.ID" />
+            <Adminbutton class="u-admin" size="mini" />
         </div>
     </div>
 </template>
 
 <script>
-import Adminbutton from '@jx3box/jx3box-common-ui/src/bread/Adminbutton.vue'
-    export default {
-        name : 'single-panel',
-        props:['post'],
-        data() {
-            return {}
+import Adminbutton from "@jx3box/jx3box-common-ui/src/bread/Adminbutton.vue";
+import Share from '@jx3box/jx3box-common-ui/src/interact/Share2.vue'
+export default {
+    name: "single-panel",
+    props: ["post"],
+    data() {
+        return {};
+    },
+    computed: {
+        title: function () {
+            return _.get(this.post, "post_title") || "无标题";
         },
-        computed:{
-            title: function() {
-                return _.get(this.post, "post_title") || "无标题";
-            },
-            authorID : function (){
-                return _.get(this.post, "post_author")
-            },
-            banner : function (){
-                return _.get(this.post, "post_banner") || "";
+        authorID: function () {
+            return _.get(this.post, "post_author");
+        },
+        banner: function () {
+            return _.get(this.post, "post_banner") || "";
+        },
+        meta : function (){
+            return {
+                title : this.title,
+                banner : this.banner,
+                desc : `作者：${this.post?.author || '匿名'}，来自：JX3BOX魔盒`,
+                summary : '一站式剑三工具资源站'
             }
-        },
-        methods:{},
-        mounted() {
-        },
-        components : {
-            Adminbutton
         }
-    }
+    },
+    methods: {},
+    mounted() {},
+    components: {
+        Adminbutton,
+        Share
+    },
+};
 </script>
 
 <style scoped lang="less">
-    @import '../assets/css/single-panel.less';
+@import "../assets/css/single-panel.less";
+.u-share{
+    .fz(13px);
+}
 </style>
